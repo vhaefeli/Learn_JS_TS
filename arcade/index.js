3e48c5ea431b1a1a28d78e475c75cdd0e860df25
Vhaefeli <vhaefeli@student.42lausanne.ch>
2023-06-07 11:35:44 +0200

    blinking text

--- arcade/index.js
+++ arcade/index.js
@@ -1,5 +1,6 @@
 const mousemove = document.querySelector(".mousemove");
 const coin = document.getElementById("coin");
+const blinkingText = document.getElementById("insert");
 
 window.addEventListener("mousemove", (e) => {
 	console.log(e);d
@@ -22,3 +23,12 @@ coin.addEventListener("mouseenter", function(event) {
 	// Déplacer l'objet de 10 pixels vers le bas en mettant à jour la position
 	coin.style.top = "37vw";
   });
+
+if (blinkingText)
+{
+	let isVisible = true;
+	setInterval(() => {
+	  isVisible = !isVisible;
+	  blinkingText.style.visibility = isVisible ? 'visible' : 'hidden';
+	}, 500);
+}
